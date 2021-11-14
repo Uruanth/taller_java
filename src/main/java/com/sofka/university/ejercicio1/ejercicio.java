61ffbbb7cc170872473fc28196165c8e37bc8911
DaironPerilla <perilla174@gmail.com>
2021-11-14 18:47:02 -0500

    v0.0.0 inicio del proyecto

---
+++ src/main/java/com/sofka/university/ejercicio1/ejercicio.java
@@ -0,0 +1,9 @@
+package com.sofka.university.ejercicio1;
+
+/**
+ * Declara 2 variables numéricas (con el valor que desees),
+ * he indica cual es mayor de los dos. Si son iguales indicarlo también.
+ * Ve cambiando los valores para comprobar que funciona.
+ */
+public class ejercicio {
+}
---
+++ src/main/java/com/sofka/university/principal.java
@@ -0,0 +1,7 @@
+package com.sofka.university;
+
+public class principal {
+    public static void main(String[] args) {
+
+    }
+}
